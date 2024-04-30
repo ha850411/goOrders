8291779a4e6d85c64a9a5a34c48fd374506dec51
eason.lee[李宜丞] <eason.lee@104.com.tw>
2024-04-30 15:33:17 +0800

    add line notify request

--- service/line/notify/LineNotify.go
+++ service/line/notify/LineNotify.go
@@ -1,7 +1,14 @@
 package notify
 
 import (
+	"fmt"
 	"goOrders/conf"
+	"goOrders/database"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 var (
@@ -15,5 +22,32 @@ var (
 func init() {
 	if conf.Settings.Common.ENV != "local" {
 		REDIRECT_URI = "http://107.191.52.212/api/line/oauth"
+	}
+}
+
+func Request(message string) {
+	form := url.Values{}
+	form.Add("message", message)
+
+	sql := "SELECT linebot_token FROM users WHERE linebot_token IS NOT NULL"
+	db := database.DbConnect()
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var token string
+		rows.Scan(&token)
+		bearerToken := fmt.Sprintf("Bearer %s", token)
+		req, _ := http.NewRequest("POST", NOTICE_URL, strings.NewReader(form.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Set("Authorization", bearerToken)
+
+		client := &http.Client{}
+		resp, _ := client.Do(req)
+		response, _ := ioutil.ReadAll(resp.Body)
+		// var jsonResponse map[string]interface{}
+		fmt.Printf("response: %v\n", string(response))
 	}
 }
